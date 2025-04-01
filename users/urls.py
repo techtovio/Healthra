d536d74767284ca68ab45d8d71dcd59dc9c591c6
Titos Kipkoech <98801777+techtovio@users.noreply.github.com>
2025-04-01 18:30:24 +0300

    Remove unnecessary files

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegisterUserView, LoginView, UserProfileView
-
-urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', UserProfileView.as_view(), name='profile'),
-]
